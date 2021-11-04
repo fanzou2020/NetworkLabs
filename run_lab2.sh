cc98d5db98f9c3ee354482a8f431e1718eca604f
Fan Zou <fanzou2020@icloud.com>
2021-11-04 16:17:37 -0400

    minor update

--- run_lab2.sh
+++ run_lab2.sh
@@ -12,28 +12,28 @@ all_cmds=("$cmd1" "$cmd2" "$cmd3" "$cmd4" "$cmd5" "$cmd6")
 for c in "${all_cmds[@]}"
 do
   read
-  echo "-----------------------------------------"
+  echo "----------------------------------------------------------------------------------"
   echo "$c"
   echo ""
   eval "$c"
 done
 
 read
-echo "-------- Writing to the same file ----------"
+echo "-------- Writing to the same file ---------------------------------------------------"
 cmd7="python httpc.py -v post 'localhost:8080/file3.txt' -d 'write from client1' -h 'append:True'"
 cmd8="python httpc.py -v post 'localhost:8080/file3.txt' -d 'write from client2' -h 'append:True'"
 eval "$cmd7"
 eval "$cmd8"
 
 read
-echo "--------- One reading, one writing ----------"
+echo "--------- One reading, one writing ---------------------------------------------------"
 cmd9="python httpc.py -v get 'localhost:8080/file4.txt'"
 cmd10="python httpc.py -v post 'localhost:8080/file4.txt' -d 'write from cmd'"
 eval "$cmd9"
 eval "$cmd10"
 
 read
-echo "--------- Two clients reading ----------"
+echo "--------- Two clients reading ---------------------------------------------------"
 cmd9="python httpc.py -v get 'localhost:8080/file5.txt'"
 cmd10="python httpc.py -v get 'localhost:8080/file5.txt'"
 eval "$cmd9"
